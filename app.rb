71c9414597b7d376ee6d0144395c44720f25e1b5
vvani06 <vvani06@gmail.com>
2014-12-31 12:42:33 +0000

    add filter code

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+before do
+	@before_value = "foo"
+	puts "Before filter called"
+end
 
 get '/' do
   'Hello Nitrous/Sinatra!'
+end
+
+get '/about' do
+	"about #{@before_value} site .."
+end
+
+after do
+	puts "After filter called"
 end
